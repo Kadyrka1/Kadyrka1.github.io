c2e5fc2ccb3c03ccb47dae47ca19790424cdf068
Aidar Kadyr <142879004+Kadyrka1@users.noreply.github.com>
2023-12-28 09:04:55 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+ 
+  <!-- Footer
+  ============================ -->
+
+<footer id="footer" class="section bg-dark footer-text-light">
+    <div class="container">
+      <ul class="social-icons social-icons-lg social-icons-muted justify-content-center mb-3">
+          <li><a href="mailto:your.email@example.com" target="_blank" title="Email"><i class="fas fa-envelope"></i></a></li>
+          <li><a href="https://wa.me/yourphonenumber" target="_blank" title="WhatsApp"><i class="fab fa-whatsapp"></i></a></li>
+          <li><a href="https://www.instagram.com/yourusername/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+          <li><a data-toggle="tooltip" href="http://www.github.com/" target="_blank" title="" data-original-title="GitHub"><i class="fab fa-github"></i></a></li>
+      </ul>
+    </div>
+  </footer>
+  <!-- Footer end -->
---
+++ sidebar.php
@@ -0,0 +1,58 @@
+ <!-- Sidebar Navigation
+	============================ -->
+	<div class="idocs-navigation bg-light">
+      <ul class="nav flex-column ">
+
+        <!--
+        <li class="nav-item"><a class="nav-link active" href="#ref_start">Overview</a>
+          <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#idocs_installation">Installation</a></li>
+            <li class="nav-item"><a class="nav-link" href="#idocs_html_structure">HTML Structure</a></li>
+          </ul>
+        </li>
+        -->
+
+        <li class="nav-item"><a class="nav-link" href="#ref1">Home</a></li>
+        <li class="nav-item"><a class="nav-link active" href="#ref2">About Me</a>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref2_1">Personal Bio</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref2_2">Professional Background</a></li>
+        </ul></li>
+        <li class="nav-item"><a class="nav-link active" href="#ref3">Skills</a>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref3_1">Technical Skills</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref3_2">Soft Skills</a></li>
+        </ul></li>
+		<li class="nav-item"><a class="nav-link active" href="#ref4">Experience</a>
+		<ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref4_1">Work History</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref4_2">Key Achievements</a></li>
+        </ul></li>
+        <li class="nav-item"><a class="nav-link active" href="#ref5">Education</a>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref5_1">Academic Qualifications</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref5_2">Certifications</a></li>
+        </ul></li>
+        <li class="nav-item"><a class="nav-link active" href="#ref6">Portfolio</a>
+        <ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref6_1">Web Development Projects</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref6_2">Personal Projects</a></li>
+        </ul></li>
+		<li class="nav-item"><a class="nav-link active" href="#ref7">Services</a>
+		<ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref7_1">Freelance Services Offered</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref7_2">Consulting Services</a></li>
+        </ul></li>
+		<li class="nav-item"><a class="nav-link active" href="#ref8">Testimonials</a>
+		<ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref8_1">Client Feedback</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref8_2">Endorsements</a></li>
+        </ul></li>
+		<li class="nav-item"><a class="nav-link active" href="#ref9">Blog</a>
+		<ul class="nav flex-column">
+            <li class="nav-item"><a class="nav-link" href="#ref9_1">Technical Articles</a></li>
+            <li class="nav-item"><a class="nav-link" href="#ref9_2">Personal Insights</a></li>
+        </ul></li>
+		<li class="nav-item"><a class="nav-link" href="#ref10">Contact</a></li>
+      </ul>
+    </div>
